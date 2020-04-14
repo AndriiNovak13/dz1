3bfc0fd2aa1590d37ba66597a291923455d2e919
AndriiNovak13 <AndriiNovak13@users.noreply.github.com>
2020-04-14 14:18:00 +0300

    change my task

---
+++ maket-grav/z29grav/js/app.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    $('.service__list-slaider').slick({
+        arrows:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        dots:false,
+        appendArrows:$('.arrow__service'),
+    
+    });
+
+
+    $('.testi__slaider').slick({
+        arrows:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        dots:false,
+        appendArrows:$('.arrow__testi'),
+    
+    });
+
+}); 
